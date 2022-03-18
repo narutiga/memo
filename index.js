366664e5ac5878bf42ea00c6f1c8ce5b8fc50645
narutiga <100295602+narutiga@users.noreply.github.com>
2022-03-18 20:41:17 +0900

    初期状態

---
+++ index.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form");
+const textarea = document.getElementById("textare");
+
+textarea.addEventListener("submit",function () {
+    console.log(textarea.value);
+});
