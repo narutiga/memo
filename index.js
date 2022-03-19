427c5f978c5bd61d5befd5f82ffd9134564b359f
narutiga <100295602+narutiga@users.noreply.github.com>
2022-03-19 16:49:29 +0900

    登録と削除のみ

--- index.js
+++ index.js
@@ -3,24 +3,43 @@ const textarea = document.getElementById("textarea");
 const ul = document.getElementById("ul");
 const button = document.getElementById("button");
 
+const memos = JSON.parse(localStorage.getItem("memos"));
 
-button.addEventListener("click",add());
-
-
-
+if(memos) {
+    memos.forEach(memo => {
+        add(memo);
+    })
+}
 
 function add(memo) {
     let memoText = textarea.value;
 
-//     if(memo) {
-//         memoText = memo.text;
-//     }
+    if(memo) {
+        memoText = memo;
+    }
 
     if(memoText) {
         const li = document.createElement("li");
         li.innerText = memoText;
+
+        li.addEventListener("contextmenu", function
+        (event) {
+            event.preventDefault();
+            li.remove();
+            saveData();
+        });
+
         ul.appendChild(li);
         textarea.value = "";
+        saveData();
     }
+}
 
+function saveData() {
+    const lists = document.querySelectorAll("li");
+    let memos = [];
+    lists.forEach(list => {
+        memos.push(list.innerText);
+    });
+    localStorage.setItem("memos",JSON.stringify(memos));
 }
