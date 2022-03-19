1672743b92f86d71b8e83bf3c4b190c0e846460e
narutiga <100295602+narutiga@users.noreply.github.com>
2022-03-19 10:06:39 +0900

    リストに追加

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const form = document.getElementById("form");
-const textarea = document.getElementById("textare");
+const textarea = document.getElementById("textarea");
+const ul = document.getElementById("ul");
+const button = document.getElementById("button");
 
-textarea.addEventListener("submit",function () {
-    console.log(textarea.value);
-});
+
+button.addEventListener("click",add());
+
+
+
+
+function add(memo) {
+    let memoText = textarea.value;
+
+//     if(memo) {
+//         memoText = memo.text;
+//     }
+
+    if(memoText) {
+        const li = document.createElement("li");
+        li.innerText = memoText;
+        ul.appendChild(li);
+        textarea.value = "";
+    }
+
+}
